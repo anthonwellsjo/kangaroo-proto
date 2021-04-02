31526e281ec925bbc142096fa321e0b5cacd8ff4
Anthon Wellsjö <anthonwellsjo@gmail.com>
2021-04-02 21:26:51 +0200

    to install express graphql

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,14 @@
 import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 async function main() {
-  // ... you will write your Prisma Client queries here
-  const allUsers = await prisma.user.findMany()
-  console.log(allUsers)
+  await prisma.user.create({
+    data: {
+      name: 'Alice',
+      email: 'alice@prisma.io',
+    },
+  })
+  const allUsers = await prisma.user.findMany({});
+  console.dir(allUsers, { depth: null })
 }
 main()
   .catch((e) => {
